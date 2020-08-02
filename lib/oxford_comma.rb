0e6fcc98129bf7d5a7dc8ce803204d9478afd1bc
Ryan Pearce <ryan.pearce@key.me>
2020-08-02 17:29:50 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
 def oxford_comma(array)
-
+  if array.size == 1 
+    array.join
+  elsif array.size == 2 
+    array.join (" and ")
+  elsif array.size > 2 
+    last_element = array.pop
+    array_to_string = array.join (", ")
+    final_joined_array = array_to_string + ", and " + last_element
+    final_joined_array
+  end
 end
+
