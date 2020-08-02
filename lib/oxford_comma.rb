b7589d382c2fa5146d82a0fe4db49ef2924d4070
Ryan Pearce <ryan.pearce@key.me>
2020-08-02 17:29:16 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -6,6 +6,6 @@ def oxford_comma(array)
   elsif array.size > 2 
     last_element = array.pop
     array_to_string = array.join (", ")
-    final_joined_array = array_to_string + " , and " + last_element
+    final_joined_array = array_to_string + ", and " + last_element
     final_joined_array
 end
